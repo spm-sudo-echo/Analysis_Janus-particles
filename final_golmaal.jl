983da45f1b5328106c06969166e5859e0e4d63b1
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2024-06-07 17:20:56 +0200

    commit from laptop

--- final_golmaal.jl
+++ final_golmaal.jl
@@ -11,7 +11,7 @@ include("temporal_crop_video.jl")
 include("mean_sqr_disp.jl")
 #Varaibales for Analysis
 diamPart=3  # mean diameter of the particles to be tracked, in microns
-um_px =  50/255 #for 800x  #100/382 for 600x         # micron to pixel convertion for Hirox microscope 
+um_px = 100/382 #for 600x  #50/255 for 800x          # micron to pixel convertion for Hirox microscope 
 framerate = 25          # fps of the video in analysis
 pixel_x=2040
 pixel_y=1530
@@ -24,11 +24,11 @@ mask_y_end=1500
 
 
 mask=falses(pixel_y,pixel_x)       # values in pixels 
-mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
+#mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
 
 #Path naming for file storage
-filename="VID005"   # name of the video to be tracked
-pathORIG="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\"   # path of the folder containing the video to be tracked
+filename="VID008"   # name of the video to be tracked
+pathORIG="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\22\\exp2\\"   # path of the folder containing the video to be tracked
 folderDEST="analysis_"*filename   # name of the folder where to store the result of the tracking
 pathDEST=pathORIG*folderDEST   # path of the folder where to store the result of the tracking
 datestamp=Dates.format(now(),"YYYY.mm.dd_HH.MM.SS")  # todays date
@@ -50,13 +50,13 @@ start_frame=1*framerate
 end_frame= 47*framerate
 crop_vid = temporal_crop_video(vid,framerate,start_frame,end_frame,filename,pathDEST) 
 vid_crop=crop_vid
-#
+
 #track particle call
-track_particles(framerate,filename,pathDEST,mask,vid_crop)
+track_particles(framerate,filename,pathDEST,mstituteask,vid_crop)
 
 #mean_sqr_disp.jl variables
 
-pathDEST= "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\analysis_VID005_2024.05.10_15.50.10\\"
+#pathDEST= "C:\\Users\\j.sharma\\OneDrivestitute - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\stituteexp1\\analysis_VID005_2024.05.10_15.50.10\\"
 #folder input corresponnds to pathDEST
 mean_sqr_disp(pathDEST,filename,framerate,um_px,diamPart)
 
