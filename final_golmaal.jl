1084b1b107f1736a11c4cf65c0312c18d4f896b5
o.tricinci <o.tricinci@santannapisa.it>
2024-09-13 16:58:53 +0200

    testing from omar computer

--- final_golmaal.jl
+++ final_golmaal.jl
@@ -12,7 +12,7 @@ include("velocity_cal.jl")
 #Varaibales for Analysis
 diamPart=3  # mean diameter of the particles to be tracked, in microns
 um_px =  50/255 #for 800x  #100/382 for 600x         # micron to pixel convertion for Hirox microscope 
-framerate = 25          # fps of the video in analysis
+framerate = 12         # fps of the video in analysis
 pixel_x=2040
 pixel_y=1530
 
@@ -27,8 +27,8 @@ mask=falses(pixel_y,pixel_x)       # values in pixels
 mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
 
 #Path naming for file storage
-filename="VID005"   # name of the video to be tracked
-pathORIG="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\"   # path of the folder containing the video to be tracked
+filename="VID001"   # name of the video to be tracked
+pathORIG="C:\\Users\\o.tricinci\\Scuola Superiore Sant'Anna\\Microscale Robotics Laboratory - RESEARCH - Research\\Data\\HRX_Hirox-microscope\\P19\\PDA activity\\exp1_2.5%\\"   # path of the folder containing the video to be tracked
 folderDEST="analysis_"*filename   # name of the folder where to store the result of the tracking
 pathDEST=pathORIG*folderDEST   # path of the folder where to store the result of the tracking
 datestamp=Dates.format(now(),"YYYY.mm.dd_HH.MM.SS")  # todays date
@@ -36,19 +36,19 @@ pathDEST=pathDEST*"_"*datestamp
 mkdir(pathDEST)
 
 println("Reading the desired video.")
-pathVID=pathORIG*filename*".wmv"
+pathVID=pathORIG*filename*".avi"
 io   = VideoIO.open(pathVID)
 vid  = VideoIO.openvideo(io)
 #video_frames = VideoIO.load(pathVID)
 
 img= first(vid)# video_frames[70]
 imshow(img)
-#=Cropping video temporally
+Cropping video temporally
 println("Cropping the video to the desired limits.")
 #start_frame=1*framerate
 start_frame=1
 #end_frame=size(collect(vid),1)
-end_frame= 48*framerate
+end_frame= 1*framerate
 crop_vid = temporal_crop_video(vid,framerate,start_frame,end_frame,filename,pathDEST) 
 vid_crop=crop_vid
 #
