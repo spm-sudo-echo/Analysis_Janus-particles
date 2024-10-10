778afb78c11a929dc5e23229456e2d52eacb300b
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2024-10-10 13:25:30 +0200

    Updates from desktop

--- final_golmaal.jl
+++ final_golmaal.jl
@@ -11,32 +11,31 @@ include("mean_sqr_disp.jl")
 include("velocity_cal.jl")
 #Varaibales for Analysis
 diamPart=3  # mean diameter of the particles to be tracked, in microns
-um_px =  50/255 #for 800x  #100/382 for 600x         # micron to pixel convertion for Hirox microscope 
-framerate = 25          # fps of the video in analysis
-pixel_x=2040
-pixel_y=1530
+um_px =  50/167 #for 800x  #100/382 for 600x         # micron to pixel convertion for Hirox microscope 
+framerate = 60          # fps of the video in analysis
+pixel_x=2248
+pixel_y=1500
 
-mask_x_start=502
+mask_x_start=2
 mask_x_end=2000
 
-mask_y_start=800
-mask_y_end=1500
-
+mask_y_start=100
+mask_y_end=1200
 
 mask=falses(pixel_y,pixel_x)       # values in pixels 
-#mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
+mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
 
 #Path naming for file storage
-filename="VID005"   # name of the video to be tracked
-pathORIG="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\"   # path of the folder containing the video to be tracked
-folderDEST="analysis_"*filename   # name of the folder where to store the result of the tracking
+filename="VID013"   # name of the video to be tracked
+pathORIG= raw"C:\Users\j.sharma\OneDrive - Scuola Superiore Sant'Anna\P10 Microfabrication\Experiments\2024\06.June\27\exp3\\"  # path of the folder containing the video to be tracked
+folderDEST="\\analysis_"*filename   # name of the folder where to store the result of the tracking
 pathDEST=pathORIG*folderDEST   # path of the folder where to store the result of the tracking
 datestamp=Dates.format(now(),"YYYY.mm.dd_HH.MM.SS")  # todays date
 pathDEST=pathDEST*"_"*datestamp
 mkdir(pathDEST)
 
 println("Reading the desired video.")
-pathVID=pathORIG*filename*".avi"
+pathVID=pathORIG*filename*".mp4"
 io   = VideoIO.open(pathVID)
 vid  = VideoIO.openvideo(io)
 #video_frames = VideoIO.load(pathVID)
@@ -46,9 +45,9 @@ imshow(img)
 #Cropping video temporally
 println("Cropping the video to the desired limits.")
 #start_frame=1*framerate
-start_frame=1*framerate
+start_frame=1 #*framerate
 #end_frame=size(collect(vid),1)
-end_frame= 2*framerate
+end_frame= 60*framerate
 crop_vid = temporal_crop_video(vid,framerate,start_frame,end_frame,filename,pathDEST) 
 vid_crop=crop_vid
 
@@ -74,4 +73,4 @@ pathDEST= "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Micro
 
 mean_sqr_disp(pathDEST,filenome,framerate,um_px,diamPart)
 =#
-velocity_cal(pathDEST,filename,diamPart)
+#velocity_cal(pathDEST,filename,diamPart)
