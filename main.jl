865c7738c7dce9a6370ebfe45f8bd596e085c0f0
yashpalsinghbrar <144710782+yashpalsinghbrar@users.noreply.github.com>
2025-11-20 22:03:55 +0100

    Main.jl was optimized and cleaned

--- main.jl
+++ main.jl
@@ -1,80 +1,71 @@
 # This is the end scrip that incorporates the call to track_paticles.jl and MSD_singVid.jl.
 #The aim is to have one script that includes all the input variables and path definitions.
 
-using Images, VideoIO, ImageView, FileIO, CSV, DataFrames, Dates, StatsPlots, CategoricalArrays, Plots, NaNStatistics, LsqFit, Statistics, JSON3, FFMPEG, BlobTracking
+using Images, VideoIO, CSV, DataFrames, Dates, Plots, NaNStatistics, Statistics, JSON3, FFMPEG, BlobTracking
 gr()  
 
 #BlobTracking.PROGRESS[1] = false
 include("track_particles.jl")
-include("temporal_crop_video.jl")
 include("mean_sqr_disp.jl")
 include("mean_sqr_disp_cal.jl")
-include("velocity_cal.jl")
-include("save_data.jl")
 include("drift_corr.jl")
-start_time=time()
+include("temporal_crop_video.jl")
+
 # This loop runs for multiple videos provided they are in the same folder and have the same magnification
-folder_path= [raw"C:\Users\y.brar\OneDrive - Scuola Superiore Sant'Anna\Work\Yashpal\2025\11\19\Control\exp3\\"]   
+folder_path = [raw"C:\Users\y.brar\OneDrive - Scuola Superiore Sant'Anna\Work\Yashpal\2025\11\19\Control\exp2\\"]   
 
 #Varaibales for Analysis
-diamPart= 3.0  # mean diameter of the particles to be tracked, in microns
-um_px =  100/382  # 50/316 for 1000x #50/255 for 800x  #100/382 for 600x #100/251 for 400x         # micron to pixel convertion for Hirox microscope 
-framerate = 25         # fps of the video in analysis
-pixel_x=2040  
-pixel_y=1530
+diamPart = 3.0  # mean diameter of the particles to be tracked, in microns
+um_px = 100/382  # 50/316 for 1000x #50/255 for 800x  #100/382 for 600x #100/251 for 400x         # micron to pixel convertion for Hirox microscope 
+framerate = 25  # fps of the video in analysis
+pixel_x = 2040
+pixel_y = 1530
+
+video_indices = 1:1  # Flexible: Adjust this range or list for multiple videos, e.g., [1,2,3,5]
+
 # This loop runs for multiple videos provided they are in the same folder and have the same magnification
+
 for folder in folder_path
     println(folder)
-    for i in 2:2
+    for i in video_indices
+        start_time = time()
 
-# mask_x_start=502
+        filename = "VID" * lpad(string(i), 3, '0')
+        datestamp = Dates.format(now(), "YYYY.mm.dd_HH.MM.SS")
+        pathDEST = folder * "\\analysis_" * filename * "_" * datestamp # path of the folder where to store the result of the tracking
+        mkdir(pathDEST)
 
-# mask_x_end=2000
+        println("Reading the desired video.")
+        pathVID = joinpath(folder, filename * ".wmv")   # path of the video to be tracked
+        io = VideoIO.open(pathVID)
+        vid = VideoIO.openvideo(io)
 
-# mask_y_start=800
-# mask_y_end=1500
-#  mask=falses(pixel_y,pixel_x)       # values in pixels 
-# mask[mask_y_start:mask_y_end,mask_x_start:mask_x_end].=true
- mask=trues(pixel_y,pixel_x)  
+        mask = trues(pixel_y, pixel_x)
 
-#Path naming for file storage
+        #Cropping video temporally
+        println("Cropping the video to the desired limits.")
+        #start_frame=1*framerate
+        start_frame=1 #*framerate
+        # #end_frame=size(collect(vid),1)
+        end_frame= 1*framerate
+        crop_vid = temporal_crop_video(vid,framerate,start_frame,end_frame,filename,pathDEST) 
+        vid_crop=crop_vid
+        VideoIO.close(vid)
 
-filename="VID00$i"
-pathORIG=folder   # name of the video to be tracked
-#pathORIG="C:\\Users\\j.sharma\\Scuola Superiore Sant'Anna\\Microscale Robotics Laboratory - DATA_2025 - DATA_2025\\Data\\HRX_Hirox-microscope\\P19\\19052025\\PDA_pt_0%\\"   # path of the folder containing the video to be tracked""C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\"   # path of the folder containing the video to be tracked
-folderDEST="analysis_"*filename   # name of the folder where to store the result of the tracking
-pathDEST=pathORIG*folderDEST   # path of the folder where to store the result of the tracking
-datestamp=Dates.format(now(),"YYYY.mm.dd_HH.MM.SS")  # todays date
-pathDEST=pathDEST*"_"*datestamp
-mkdir(pathDEST)
+        println("Tracking the particles.")
+        #track_particles(framerate, filename, pathDEST, mask, vid)
+        track_particles(framerate, filename, pathDEST, mask, vid_crop)
 
-println("Reading the desired video.")
-pathVID=pathORIG*filename*".wmv"   # path of the video to be tracked
-io   = VideoIO.open(pathVID)
-vid  = VideoIO.openvideo(io)
-#video_frames = VideoIO.load(pathVID)
+        println("Calculating MSD.")
+        mean_sqr_disp(pathDEST, filename, framerate, um_px, diamPart)
 
-img= first(vid)# video_frames[70]
-imshow(img)
-# #Cropping video temporally
-# println("Cropping the video to the desired limits.")
-#start_frame=1*framerate
-# start_frame=1 #*framerate
-# #end_frame=size(collect(vid),1)
-#end_frame= 2*framerate
-#crop_vid = temporal_crop_video(vid,framerate,start_frame,end_frame,filename,pathDEST) 
-   #vid_crop=crop_vid
-# VideoIO.close(vid)
-println("Tracking the particles.")
-track_particles(framerate,filename,pathDEST,mask,vid)
-#pathDEST= "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P10 Microfabrication\\Experiments\\2024\\05.May\\07\\exp1\\analysis_VID005_2024.05.10_15.50.10\\"
-#folder input corresponnds to pathDEST
-println("Calculating MSD.")
-mean_sqr_disp(pathDEST,filename,framerate,um_px,diamPart)
-end_time=time()
-println("Analysis time: ", end_time-start_time)
+        VideoIO.close(io)
+        end_time = time()
+        println("Analysis time: ", end_time - start_time)
+    end
 end
-end
+
+
 #fitting_linear(pathDEST,filename,diamPart)
 
 # filenome="VID001"   # name of the video to be tracked
@@ -85,4 +76,3 @@ end
 # #folder input corresponnds to pathDEST
 # mean_sqr_disp(pathDEST,filenome,framerate,um_px,diamPart)
 #velocity_cal(pathDEST,filename,diamPart)
-
